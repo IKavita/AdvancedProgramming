922dce0b735bc98f925bd6fc5b142fe6c051bb46
Kavita Sharma <kavita.sharma@sky.uk>
2020-05-17 13:12:17 +0100

    implemented game component class

--- AdvancedProgramming-Task1/gameComponent.h
+++ AdvancedProgramming-Task1/gameComponent.h
@@ -1,7 +1,18 @@
 #pragma once
+#include <iomanip>
 
-
-class gameComponent {
-
-
+class GameComponent {
+	public:
+		GameComponent() {
+			id = ++instances; // will give a unique id number each time
+		};
+		virtual void Update(const tm* time) {
+			std::cout << "ID : " << id << " Updated @ " << std::put_time(time, "%X") << std::endl; // printing id and current time
+		};
+	private:
+		// initialising variables
+		int id = 0;
+		static int instances; // total number of instances instantiated from two component classes
 };
+
+int GameComponent::instances;
